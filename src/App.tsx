f0f18b2ce5d49a5e64302e3ddd32fa7d2b63ca4b
kiranbansode <kiran5120135@gmail.com>
2023-02-09 15:54:08 +0530

    add info about project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,51 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
-	const [count, setCount] = useState(0);
+	return (
+		<div className="app-root">
+			<h1>Hii! Welcome to Kiran's Resume 😎 </h1>
+			<h2>Why I'm seeing this? 😳🕶🤏</h2>
+			<p>
+				Currently this project is still under planning 📝 phase. You will see
+				MVP of this project when I complete my previous&nbsp;
+				<a href="https://github.com/kiranbansode/votesub" target="_blank">
+					VoteSub
+				</a>
+				&nbsp;project.
+			</p>
 
-	return <div className="App">Hello 😄</div>;
+			<p>
+				You can find resources related to this project on it's&nbsp;
+				<a href="https://github.com/kiranbansode/resume" target="_blank">
+					GitHub
+				</a>
+				&nbsp;repository.
+			</p>
+
+			<h2>🗓️ Goals Behind building this project</h2>
+			<ul>
+				<li>
+					learn and practice TDD(Test Driven Development) concepts ⚡ which I
+					was unable to while developing my VoteSub project
+				</li>
+				<li>learn and implement new React ⚛️ Concepts</li>
+				<li>implement CSS BEM model 🧱 (no Sass this time)</li>
+				<li>implement CSS Animations ✴️ (without any third party library)</li>
+				<li>
+					use modern ways of implementing Responsive Web Design 🖥️ ➤ 💻 ➤ 📱
+				</li>
+				<li>learn JIRA 🕵️ (because it became industry standard)</li>
+				<li>
+					strictly follow SDLC (Software Development Life Cycle) and Agile
+					Methodologies 👨‍🍳
+				</li>
+				<li>
+					serve as a goto place 🔎 where any one can learn and find updated
+					information about me like physical resume.
+				</li>
+			</ul>
+		</div>
+	);
 }
 
 export default App;
