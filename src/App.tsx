2977c7c078177d74a51921b940ff2c6b254e1c19
Kiran Bansode <kiran5120135@gmail.com>
2023-05-04 12:32:28 +0530

    feat(App): add passing frontend code for App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,24 @@
-import "./App.css";
+import Intro from './components/Intro';
+import Experiences from './components/Experiences';
+import Skills from './components/Skills';
+import Contact from './components/Contact';
+import Courses from './components/Courses';
+import Education from './components/Education';
+import Tools from './components/Tools';
+
+import './App.css';
 
 function App() {
 	return (
 		<div className="app-root">
-			<h1>Hii! Welcome to Kiran's Resume 😎 </h1>
-			<h2>
-				Where is your resume? <span className="remove-glasses">😳🕶🤏</span>
-			</h2>
-			<p>
-				Currently this project is still under planning 📝 phase. You will see
-				MVP of this project when I complete my previous&nbsp;
-				<a href="https://github.com/kiranbansode/votesub" target="_blank">
-					VoteSub
-				</a>
-				&nbsp;project.
-			</p>
-
-			<p>
-				You can find resources related to this project on it's&nbsp;
-				<a href="https://github.com/kiranbansode/resume" target="_blank">
-					GitHub
-				</a>
-				&nbsp;repository.
-			</p>
-
-			<h2>🗓️ Goals behind building this project</h2>
-			<ul>
-				<li>
-					learn and practice TDD(Test Driven Development) concepts ⚡ which I
-					was unable to while developing my VoteSub project
-				</li>
-				<li>learn and implement new React ⚛️ Concepts</li>
-				<li>implement CSS BEM model 🧱 (no Sass this time)</li>
-				<li>implement CSS Animations ✴️ (without any third party library)</li>
-				<li>
-					use modern ways of implementing Responsive Web Design 🖥️ ➤ 💻 ➤ 📱
-				</li>
-				<li>learn JIRA 🕵️ (because it became industry standard)</li>
-				<li>
-					strictly follow SDLC (Software Development Life Cycle) and Agile
-					Methodologies 👨‍🍳
-				</li>
-				<li>
-					serve as a goto place 🔎 where any one can learn and find updated
-					information about me like physical resume.
-				</li>
-			</ul>
+			<div className="background__wrapper"></div>
+			<Intro />
+			<Experiences />
+			<Skills />
+			<Tools />
+			<Courses />
+			<Education />
+			<Contact />
 		</div>
 	);
 }
